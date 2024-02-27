19351d0cd21617b82eb24441454c55590528f035
Enrique Chan <chandiazenrique@gmail.com>
2024-02-27 10:10:12 -0600

    segunda version, agregue el nombre de mis compañeros

--- Print.java
+++ Print.java
@@ -3,5 +3,6 @@ package PracticaGit;
 public class Print {
 	public static void main(String args[]) {
 		System.out.println("Hi, mi name is Enrique");
+		System.out.println("Mis compañeros son Guidnell Perez, Ricardo Chab, Angel Nah, Diego Avilez y Erick Villanueva");
 	}
 }
